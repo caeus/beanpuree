c8667da260712557e8f66f1142fa7102a031b77c
caeus <camilo.a.navas@gmail.com>
2019-09-02 16:06:39 -0500

    Include inherited members again

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -4,11 +4,14 @@ import com.github.caeus.beanpuree.{Child, LabelledBeanGeneric}
 import shapeless._
 import com.github.caeus.beanpuree._
 import org.scalatest._
+import shapeless.ops.record.Keys
 
 class HelloSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
 
-     println( BeanGeneric[Child](BeanGeneric.materialize[Child,shapeless.::[String, shapeless.HNil]]))
+    val x = LabelledBeanGeneric[Child]
+    val keys= Keys[x.Repr]
+    println( keys.apply())
 
 
 
